7ae43ed5babeb88c6d923c1fe36f23eb7add6682
yamatowani <festajaune.18637@icloud.com>
2025-08-30 14:43:35 +0900

    Add prisma Schema

--- lib/quick-pitch-server-stack.ts
+++ lib/quick-pitch-server-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class QuickPitchServerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'QuickPitchServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fn = new NodejsFunction(this, "lambda", {
+      entry: "lambda/index.ts",
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_22_X,
+    });
+    const fnUrl = fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+    new cdk.CfnOutput(this, "lambdaUrl", {
+      value: fnUrl.url!,
+    });
   }
 }
